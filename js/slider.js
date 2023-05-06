558c97146d6e66aa63e5a43b7f46f63bc6610402
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2023-05-05 21:13:05 -0300

    Update

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+$('.sliderun').slick({
+    arrows: true,
+    dots: true,
+    speed:1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover:false,
+    /*responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+                }
+                
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+        ],*/
+    });
